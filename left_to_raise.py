8a0a07803a838e15dc2137995d87366cfa9baaf2
Bennet Yee <bennet.yee@gmail.com>
2024-06-28 23:22:16 -0700

    new issuance API scraper for reg CF left to raise

---
+++ left_to_raise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import json
+import requests
+import sys
+
+url='https://api.issuance.com/api/investments/summary/?slug=aptera-regcf'
+raise_goal = 5_000_000
+
+def fetch(url):
+    json_str = requests.get(url)
+    response = json.loads(json_str.content)
+    return response
+
+def left_to_raise(url):
+    d = fetch(url)
+    return raise_goal - d['total_amount_committed']['amount']
+
+def main(argv):
+    # no argument parser for now
+    print(f'{left_to_raise(url)} left to raise')
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
